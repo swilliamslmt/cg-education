4f71ac2305c027423f63222d9aac75228d5a06bd
stephanie williams <swilliamslmt@me.com>
2019-01-16 11:25:06 -0600

    section one

---
+++ README.md
@@ -0,0 +1,9 @@
+# CG Education - JavaScript
+
+HTML and CSS skeleton for CoderGirl 2018's final JavaScript project.
+* Flexbox-based layout with responsive web design and animations
+
+### Authors ###
+
+* Annie McCance, anniemccance@gmail.com
+* Christine Stavridis, castavridis@gmail.com
---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("Hello world!");
+
+let teacherName='Ms.Smith';
+let departmentName='art';
+let ratingOne= 4.0;
+let ratingTwo= 2.5;
+let ratingThree= 5.0;
+let avgRating= 'Avg Rating:3.833';
+console.log("Teacher: " + teacherName);
+console.log("Department: " + departmentName);
+console.log("Ratings: " + ratingOne + "," + ratingTwo + "," + ratingThree + "," + avgRating);
+
+
+let studentName= "Will Smith";
+let studentMajor= "Biology";
+let studentEmail= "Welcometomiami@icloud.com";
+let gpa=4.0;
+let courseOne= "BIO 101";
+let courseTwo="BIO 102";
+
+console.log("Student Name:" + studentName);
+console.log("Student Major:" + studentMajor);
+console.log("Student Email:"+ studentEmail);
+console.log("GPA:"+ gpa);
+console.log("Courses:" + courseOne + "," + courseTwo);
+
+let courseName="Art";
+let departmentNameTwo="3D design";
+let teacher="Mr.Van Gogh";
+let semester="Fall";
+
+console.log("Course Name:" + courseName);
+console.log("Department Name:"+ departmentNameTwo);
+console.log("Teacher:"+ teacher);
+console.log("Semester:"+semester);
